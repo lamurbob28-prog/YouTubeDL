066746fb60ec9e577d43ce9a333cf35c5bbd7fce
lamurbob28-prog <lamurbob28@gmail.com>
2026-06-15 11:47:07 -0400

    Set Android Java target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,15 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     packaging {
         jniLibs.useLegacyPackaging = true
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
